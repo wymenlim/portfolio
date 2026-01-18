8212ce629a833f03096e7f4853e9c503b56a7197
wymenlim <lim.wymen@gmail.com>
2026-01-18 21:42:31 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -10,10 +10,10 @@ export default function Home() {
   return (
     <main>
       <NavBar></NavBar>
-      <section id="home" className="min-h-screen bg-stone-500">
-        <div className="flex item-center justify-center md:grid place-items-center min-h-screen">
+      <section id="home" className="min-h-screen bg-contain bg-cover bg-[url('/black-bg.jpg')]">
+        <div className="flex items-center justify-center md:grid place-items-center min-h-screen">
           <div className="flex flex-col items-center gap-4">
-            <h1 className="text-white font-bold text-2xl md:text-6xl">Wymen's e-Portfolio</h1>
+            <h1 className="text-white font-bold text-2xl md:text-6xl">Wymen's Projects</h1>
             <h2 className="text-white">Scroll or use the Navigation Bar to view</h2>
           </div>
           
